f2f244a8c29f22e22fb3801f22c5d4a4db062f33
Habib Maksoud <habibmaksoud@gmail.com>
2023-03-02 00:07:45 -0800

    fixed github url for engineer, some housekeeping, finalized tests and added server's first self created website

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -13,6 +13,7 @@ const generateHTML = (teamMembers) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile Generator</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha512-9s1SRVxXUqCzoO9sRmIhJQzPyyKLCvXf4JGfFkhTJtC2xgCpBbI7W3qMgPq+qNhXc4hB9B0d2I7Ot3tfYnn8pQ==" crossorigin="anonymous" />
         <link rel="stylesheet" href="style.css">
     </head>
@@ -33,6 +34,7 @@ const generateHTML = (teamMembers) => {
                 </div>
             </div>
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
     </html>
     `;
